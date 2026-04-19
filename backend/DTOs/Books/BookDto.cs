06766beb2178b5e9d612842315157bca5a17d1bc
Merhawit Mebrahtu <merhawitristet431@gmail.com>
2026-04-19 18:20:31 +0200

    Add BookDto,CreateBookDto and UpdateBookDto

---
+++ backend/DTOs/Books/BookDto.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.DTOs.Books;
+
+public class BookDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public DateTime PublishedDate { get; set; }
+}
---
+++ backend/DTOs/Books/UpdateBookDto.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.DTOs.Books;
+
+public class UpdateBookDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public DateTime PublishedDate { get; set; }
+}
