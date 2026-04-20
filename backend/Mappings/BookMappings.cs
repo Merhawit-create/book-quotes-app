9e3bc1e9fd323f53ce28077e3e7e62067a5b7af0
Merhawit Mebrahtu <merhawitristet431@gmail.com>
2026-04-20 20:49:36 +0200

    Add mapping extensions for converting between entities and DTOs

---
+++ backend/Mappings/BookMappings.cs
@@ -0,0 +1,36 @@
+﻿using backend.DTOs.Books;
+using backend.Entities;
+
+namespace backend.Mappings;
+
+public static class BookMappings
+{
+    public static BookDto ToDto(this Book book)
+    {
+        return new BookDto
+        {
+            Id = book.Id,
+            Title = book.Title,
+            Author = book.Author,
+            PublishedDate = book.PublishedDate
+        };
+    }
+
+    public static Book ToEntity(this CreateBookDto dto, string userId)
+    {
+        return new Book
+        {
+            Title = dto.Title,
+            Author = dto.Author,
+            PublishedDate = dto.PublishedDate,
+            UserId = userId
+        };
+    }
+
+    public static void ApplyUpdate(this Book book, UpdateBookDto dto)
+    {
+        book.Title = dto.Title;
+        book.Author = dto.Author;
+        book.PublishedDate = dto.PublishedDate;
+    }
+}
---
+++ backend/Mappings/QuoteMappings.cs
@@ -0,0 +1,33 @@
+﻿using backend.DTOs.Quotes;
+using backend.Entities;
+
+namespace backend.Mappings;
+
+public static class QuoteMappings
+{
+    public static QuoteDto ToDto(this Quote quote)
+    {
+        return new QuoteDto
+        {
+            Id = quote.Id,
+            Text = quote.Text,
+            Author = quote.Author
+        };
+    }
+
+    public static Quote ToEntity(this CreateQuoteDto dto, string userId)
+    {
+        return new Quote
+        {
+            Text = dto.Text,
+            Author = dto.Author,
+            UserId = userId
+        };
+    }
+
+    public static void ApplyUpdate(this Quote quote, UpdateQuoteDto dto)
+    {
+        quote.Text = dto.Text;
+        quote.Author = dto.Author;
+    }
+}
